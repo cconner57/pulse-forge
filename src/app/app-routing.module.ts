c3b1cec092b555a42c6482121950f3fa9e706107
cconner57 <chris.conner57@gmail.com>
2023-10-25 17:13:08 -0700

    init setup and 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,89 @@
 import { NgModule } from '@angular/core';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ECommerceComponent } from './pages/e-commerce/e-commerce.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { FileManagerComponent } from './pages/file-manager/file-manager.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { MoneyComponent } from './pages/money/money.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { ProjectManagementComponent } from './pages/project-management/project-management.component';
+import { TaskBoardComponent } from './pages/task-board/task-board.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calendar',
+    component: CalendarComponent,
+    title: 'Calendar',
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
+    title: 'Chat',
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    title: 'Contacts page',
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+    title: 'Customers page',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard page',
+  },
+  {
+    path: 'store',
+    component: ECommerceComponent,
+    title: 'Store page',
+  },
+  {
+    path: 'files',
+    component: FileManagerComponent,
+    title: 'File Manager page',
+  },
+  {
+    path: 'inbox',
+    component: InboxComponent,
+    title: 'Inbox page',
+  },
+  {
+    path: 'money',
+    component: MoneyComponent,
+    title: 'Money page',
+  },
+  {
+    path: 'Notes',
+    component: NotesComponent,
+    title: 'Notes page',
+  },
+  {
+    path: 'projects',
+    component: ProjectManagementComponent,
+    title: 'Project Management page',
+  },
+  {
+    path: 'tasks',
+    component: TaskBoardComponent,
+    title: 'Task Board page',
+  },
+  {
+    path: '**',
+    component: ErrorComponent,
+    title: '404 Error page',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { FileManagerComponent } from './pages/file-manager/file-manager.component';
+import { ProjectManagementComponent } from './pages/project-management/project-management.component';
+import { TaskBoardComponent } from './pages/task-board/task-board.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ECommerceComponent } from './pages/e-commerce/e-commerce.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MoneyComponent } from './pages/money/money.component';
+import { CustomersComponent } from './pages/customers/customers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ErrorComponent, ContactsComponent, CalendarComponent, NotesComponent, FileManagerComponent, ProjectManagementComponent, TaskBoardComponent, InboxComponent, ChatComponent, ECommerceComponent, DashboardComponent, MoneyComponent, CustomersComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
