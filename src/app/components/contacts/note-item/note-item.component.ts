17237587b13d31eb64b84c66ffad710bf8ddb68d
cconner57 <chris.conner57@gmail.com>
2023-11-03 14:52:16 -0700

    notes layout finished

--- src/app/components/contacts/note-item/note-item.component.ts
+++ src/app/components/contacts/note-item/note-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Note {
+  date: string;
+  description: string;
+  id: number;
+  pinned: boolean;
+  title: string;
+}
 
 @Component({
   selector: 'app-note-item',
   templateUrl: './note-item.component.html',
-  styleUrls: ['./note-item.component.css']
+  styleUrls: ['./note-item.component.css'],
 })
 export class NoteItemComponent {
-
+  @Input() note?: Note;
 }
--- src/app/pages/notes/notes.component.ts
+++ src/app/pages/notes/notes.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { notes } from '../../mock/notes';
+
+interface Note {
+  date: string;
+  description: string;
+  id: number;
+  pinned: boolean;
+  title: string;
+}
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.css']
+  styleUrls: ['./notes.component.css'],
 })
 export class NotesComponent {
-
+  notes: Note[] = notes;
 }
